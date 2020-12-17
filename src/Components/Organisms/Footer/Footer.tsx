2ff5cb33ceb0333479aaae37098a8004f2c4bf8f
田村 充 <takashi.tamura@tamuranoAir.elecom>
2020-12-17 13:49:34 +0900

    Removed tw icon, added mail icon

--- src/Components/Organisms/Footer/Footer.tsx
+++ src/Components/Organisms/Footer/Footer.tsx
@@ -3,20 +3,20 @@ import * as S from "./style";
 import { messages } from "../../../Constants/Strings";
 import FaIconTemplate from "../../Atoms/FaIcons/FaIconTemplate";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-const Footer: React.FC = () => {
+const Footer: React.FC = () => {  
   return (
     <S.Layout>
       <S.StyledCopy>
         &copy;{messages.copyRight}
       </S.StyledCopy>
 
-      <S.StyledIcon
-        href="https://twitter.com/@takashi_t_20"
-        target="_blank"
-      >
-        <FaIconTemplate iconName={faTwitter}/>
-      </S.StyledIcon>
+      <a
+      href="mailto:takashi.t.abc.20@gmail.com"
+      target="_blank">
+        <FaIconTemplate iconName={faEnvelope}/>
+        </a>
     </S.Layout>
   )
 }
