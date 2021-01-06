69ffc80ee5b418fc5962c5dba1a971d31c82a862
田村 充 <takashi.tamura@tamuranoAir.elecom>
2021-01-06 12:10:55 +0900

    add action.test.ts

--- src/Redux/Graph/test/action.test.ts
+++ src/Redux/Graph/test/action.test.ts
@@ -1,5 +1,5 @@
 import { GraphActions } from "../action"
-import { SELECTCOUNTRY } from '../types'
+import { SELECTCOUNTRY, SELECT_UPDATE,  ADDCOUNTRY, DELCOUNTRY, DELETE_UPDATE } from '../types'
 
 describe("actionのテスト", () => {
 
@@ -12,6 +12,69 @@ describe("actionのテスト", () => {
       }
     }
     const actual = GraphActions.SelectCountry(country)
+    expect(actual).toStrictEqual(action)
+  })
+
+  test("SelectUpdateアクションテスト", () => {
+    const country = "japan"
+    const values = [{
+        Age: "",
+        Country: country,
+        Population: 1000,
+        Year: 2020,
+        docId: ""
+    }]
+    const action = {
+      type: SELECT_UPDATE,
+      payload: {
+        country,
+        values
+      }
+    }
+    const actual = GraphActions.SelectUpdate(country, values)
+    expect(actual).toStrictEqual(action)
+  })
+
+  test("AddCountryアクションのテスト", () => {
+    const country = "japan"
+    const action = {
+      type: ADDCOUNTRY,
+      payload: {
+        country
+      }
+    }
+    const actual = GraphActions.AddCountry(country)
+    expect(actual).toStrictEqual(action) 
+  })
+
+  test("DeleteCountryアクションのテスト", () => {
+    const country = "japan"
+    const action = {
+      type: DELCOUNTRY,
+      payload: {
+        country
+      }
+    }
+    const actual = GraphActions.DelCountry(country)
+    expect(actual).toStrictEqual(action) 
+  })
+
+  test("DeleteUpdateアクションのテスト", () => {
+    const country = "japan"
+    const values = {"japan": [{
+      Age: "",
+      Country: country,
+      Population: 1000,
+      Year: 2020,
+      docId: ""
+    }]}
+    const action = {
+      type: DELETE_UPDATE,
+      payload: {
+        country
+      }
+    }
+    const actual = GraphActions.DeleteUpdate(values)
     expect(actual).toStrictEqual(action)
   })
 
